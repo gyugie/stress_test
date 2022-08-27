d862d1ea27968f7bd4567d66492895fa057e3625
gyugie <mugypleci@gmail.com>
2022-08-27 19:25:07 +0700

    add new froutes loader verification

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('loaderio-ffb82307d325843dcb5df330ae437dab.txt', function () {
+    return view('welcome');
+});
