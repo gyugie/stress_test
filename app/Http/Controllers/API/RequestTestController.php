c941088e97f57bfcb36552a1e093a68f72cdc7cf
gyugie <mugypleci@gmail.com>
2022-08-27 21:33:59 +0700

    comment delay response

--- app/Http/Controllers/API/RequestTestController.php
+++ app/Http/Controllers/API/RequestTestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Actions\RandomProcessSleepAction;
 use App\Http\Controllers\Controller;
 use App\Jobs\ProcessingTestCpuJob;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -19,10 +20,13 @@ class RequestTestController extends Controller
     public function __invoke(Request $request)
     {
         try {
-            $sleep_time = (new RandomProcessSleepAction())->run(1,15, 1);
+            $at = Carbon::now();
+            $memory = memory_get_usage() / 1024;
+            $formatted = number_format($memory, 3) . 'K';
+//            $sleep_time = (new RandomProcessSleepAction())->run(1,15, 1);
             ProcessingTestCpuJob::dispatch();
-            Log::info( "RequestTestController send response with  ($sleep_time) seconds");
-            return response()->json(['sleep_time' => $sleep_time ]);
+            Log::info( "RequestTestController Current memory usage : {$formatted}\n");
+            return response()->json("OK");
         } catch (\Exception $exception) {
             Log::error( "Error RequestTestController ". $exception->getMessage());
             return response()->json(['error' => $exception->getMessage()], 500);
